651c3bc3fff393b63a2e24521b78f3d3904f4b4e
Filip Krzemień <filipos2003@gmail.com>
2023-05-10 20:29:49 +0200

    Added theme switcher redux slice and theme state images

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import chatSlice from "./chat-slice";
+import themeSlice from "./theme-slice";
 
 const store = configureStore({
     reducer: {
         chat : chatSlice.reducer , 
+        theme : themeSlice.reducer ,
     }
 });
 
